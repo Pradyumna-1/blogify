1e2b812bf1fa9947e56f72c05cadb9c12ca8cebb
Pradyumna-1 <pradyumnakumar35860@gmail.com>
2024-04-08 10:13:25 +0530

    use authentication & blog

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const userRoute = require("./routes/user");
+const Blog = require("./models/blog");
+const blogRoute = require("./routes/blog");
 
+const { checkAuthenticationCookie } = require("./middlewares/authentication");
 const PORT = 3000;
 
 mongoose
@@ -14,10 +18,19 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkAuthenticationCookie("token"));
 
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({}).sort("createAt", -1);
+  res.render("home",{
+    user:req.user,
+    blogs:allBlogs
+  })
+});
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { user: req.user });
 });
 
 app.get("/", (req, res) => {
@@ -25,5 +38,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", userRoute);
+
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
